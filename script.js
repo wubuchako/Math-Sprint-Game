8077211390ff1a3c252354e12429f0b9cf739595
wubuchako <56101474+wubuchako@users.noreply.github.com>
2020-10-11 11:39:32 +0900

    automatic scrolling and player input
    コースを選択し、計算のright and wrong ボタンを入力すると、自動でスクロールする。

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ const playAgainBtn = document.querySelector('.play-again');
 // Equations
 let questionAmount = 0;
 let equationsArray = [];
+let playerGuessArray = [];
 
 // Game Page
 let firstNumber = 0;
@@ -31,6 +32,25 @@ const wrongFormat = [];
 // Time
 
 // Scroll
+let valueY = 0; //right wrong ボタンで数字変わる
+
+// Scroll, Store user selection in playerGuessArray
+function select(guessedTrue){
+    console.log('player guess array:', playerGuessArray);
+ //Scroll 80 pixels
+ valueY += 80;
+ itemContainer.scroll(0, valueY);
+ // Add player guess to array  wrongボタン押したらfalse
+ return guessedTrue ? playerGuessArray.push('true'): playerGuessArray.push('false');
+
+}
+
+
+// Display Game Page
+function showGamePage(){
+    gamePage.hidden = false;
+    countdownPage.hidden = true;
+}
 
 // Get Random Number up to a max number
 function getRandomInt(max){
@@ -62,35 +82,51 @@ function createEquations() {
     wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`;
     wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`;
     wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`;
-    const formatChoice = getRandomInt(3);
+    const formatChoice = getRandomInt(3); //最大数を３とする
     const equation = wrongFormat[formatChoice];
     equationObject = { value: equation, evaluated: 'false' };
     equationsArray.push(equationObject);
   }
   shuffle(equationsArray);
-  console.log('equations array:', equationsArray);
+}
+
+// Add Equations to DOM
+function equationsToDOM(){
+//elementを作ってつける
+ equationsArray.forEach((equation) => {
+ // Item
+ const item = document.createElement('div');
+ item.classList.add('item');
+ // Equation Text
+ const equationText = document.createElement('h1');
+ equationText.textContent = equation.value;
+// Append
+item.appendChild(equationText);
+itemContainer.appendChild(item);
+ });
 }
 
 // Dynamically adding correct/incorrect equations
-// function populateGamePage() {
-//   // Reset DOM, Set Blank Space Above
-//   itemContainer.textContent = '';
-//   // Spacer
-//   const topSpacer = document.createElement('div');
-//   topSpacer.classList.add('height-240');
-//   // Selected Item
-//   const selectedItem = document.createElement('div');
-//   selectedItem.classList.add('selected-item');
-//   // Append
-//   itemContainer.append(topSpacer, selectedItem);
+function populateGamePage() {
+  // Reset DOM, Set Blank Space Above
+  itemContainer.textContent = '';
+  // Spacer
+  const topSpacer = document.createElement('div');
+  topSpacer.classList.add('height-240');
+  // Selected Item
+  const selectedItem = document.createElement('div');
+  selectedItem.classList.add('selected-item');
+  // Append
+  itemContainer.append(topSpacer, selectedItem);
 
-//   // Create Equations, Build Elements in DOM
-
-//   // Set Blank Space Below
-//   const bottomSpacer = document.createElement('div');
-//   bottomSpacer.classList.add('height-500');
-//   itemContainer.appendChild(bottomSpacer);
-// }
+  // Create Equations, Build Elements in DOM
+    createEquations();
+    equationsToDOM();
+  // Set Blank Space Below
+  const bottomSpacer = document.createElement('div');
+  bottomSpacer.classList.add('height-500');
+  itemContainer.appendChild(bottomSpacer);
+}
 
 
 // Displays 3, 2, 1, GO! カウントダウン表示 1秒づつズレて表示
@@ -112,7 +148,8 @@ function showCountdown(){
     countdownPage.hidden = false;
     splashPage.hidden = true;
     countdownStart();
-    createEquations();
+    populateGamePage();
+    setTimeout(showGamePage, 400);
 }
 
 
